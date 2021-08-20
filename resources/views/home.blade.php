eaa0e3b95e63f71f6ef61f7c72986ab298503d9f
ahsanhabib313 <ahsanhabib313@gmail.com>
2021-08-20 12:55:28 +0600

    presentaiton left section has been completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,8 +44,45 @@
             </a>
           </div>
       </div>
+   </div>
 </section>
-{{----}}
+
+    <section id="presentation">
+        <div class="row">
+            <div class="col-md-8 " >
+                <div class="my-3 p-3 bg-white rounded left-section">
+                    <h1 class="pb-2 font-weight-bold border-bottom border-gray">Welcome to HE&DF </h1>
+                    <p class="text-justify">
+                        The Health Education & Development Foundation(HE&DF) is a statutory body with the responsibility
+                        of establishing and maintaining high standards of medical education and recognition
+                        of medical qualifications in Bangladesh. It registers doctors to practice in Bangladesh,
+                        in order to protect and promote the health and safety of the public by ensuring
+                        proper standards in the practice of medicine.
+                    </p>
+
+                    <div class="row text-center">
+                        <div class="col">
+                            <img class="rounded-circle border shadow-lg mb-4" src="https://www.bmdc.org.bd/cis/web/img/photo_president.png" alt width="140" height="140">
+                            <h4 class="web-header font-weight-bold">Prof. Mohammad Sahidullah</h4>
+                            <p class="font-weight-bold">President</p>
+                            <p>Health Education & Development Foundation </p>
+                        </div>
+                        <div class="col">
+                            <img class="rounded-circle border shadow-lg mb-4" src="https://www.bmdc.org.bd/cis/web/img/photo_president.png" alt width="140" height="140">
+                            <h4 class="web-header font-weight-bold">Prof. Mohammad Sahidullah</h4>
+                            <p class="font-weight-bold">President</p>
+                            <p>Health Education & Development Foundation </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+
+            </div>
+
+        </div>
+
+    </section>
 
 
 
