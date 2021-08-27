7257a2605bd132f5d667d21758f683c35d2af88e
ahsanhabib313 <ahsanhabib313@gmail.com>
2021-08-27 10:36:54 +0600

    froentend task almost completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,20 +17,9 @@
    <div class="row">
       <div class="col-md-12">
          
-         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
-          {{--   <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol> --}}
+         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
               <div class="carousel-item active">
-               <img src="../img/carousel.png" class="d-block w-100" alt="carousel Image">
-              </div>
-              <div class="carousel-item">
-               <img src="../img/carousel.png" class="d-block w-100" alt="carousel Image">
-              </div>
-              <div class="carousel-item">
                <img src="../img/carousel.png" class="d-block w-100" alt="carousel Image">
               </div>
             </div>
@@ -47,10 +36,10 @@
    </div>
 </section>
 
-    <section id="presentation">
+    <section id="presentation " class="border-bottom">
         <div class="row">
             <div class="col-md-8 " >
-                <div class="my-3 p-3 bg-white rounded left-section">
+                <div class="my-3 p-3 bg-white rounded shadow-sm presentation-left-section">
                     <h1 class="pb-2 font-weight-bold border-bottom border-gray">Welcome to HE&DF </h1>
                     <p class="text-justify">
                         The Health Education & Development Foundation(HE&DF) is a statutory body with the responsibility
@@ -77,11 +66,95 @@
                 </div>
             </div>
             <div class="col-md-4">
+                <div class="my-3 p-3 bg-white rounded box-shadow shadow-sm">
+
+                    <h5 class="border-bottom border-gray pb-2 mb-0 text-muted font-weight-bold">Recent News/Updates</h5>
+                    <div class="news-scroll">
+
+                        <div class="card" >
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">An item</li>
+                                <li class="list-group-item">A second item</li>
+                                <li class="list-group-item">A third item</li>
+                                <li class="list-group-item">An item</li>
+                                <li class="list-group-item">A second item</li>
+                                <li class="list-group-item">A third item</li>
+                                <li class="list-group-item">An item</li>
+                                <li class="list-group-item">A second item</li>
+                                <li class="list-group-item">A third item</li>
+                            </ul>
+                        </div>
+                        <div class="float-right mt-4 mb-4"><a href="#">All Updates</a></div>
+
+                    </div>
+
+                </div>
 
             </div>
 
         </div>
 
+    </section>
+
+    <section id="curriculum" class="p-4">
+        <div class="row">
+            <div class="col-12">
+                <h5 class="m-1 curriculum-heading mb-3 font-weight-bold"> HE&DF Curriculums</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-width  box-shadow border border bg-light">
+                    <div class="card-body">
+                     <img class="card-img-top" src="../img/book.png" alt="Card image cap" width="20" height="20">
+                      <h5 class="card-title mt-4 news-title ">HE&DF MBBS Curriculum Update- 2012</h5>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <div><a href="#"><i class="fas fa-download"></i></a> </div>
+                        <div><a href="#">Read More</a></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card  box-shadow border border bg-light" >
+                    <div class="card-body">
+                        <img class="card-img-top" src="../img/book.png" alt="Card image cap" width="20" height="20">
+                        <h5 class="card-title mt-4 news-title">BM&DC MBBS Curriculum Update- 2012</h5>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <div><a href="#"><i class="fas fa-download"></i></a> </div>
+                        <div><a href="#">Read More</a></div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-3">
+                <div class="card  box-shadow border border bg-light">
+                    <div class="card-body">
+                        <img class="card-img-top" src="../img/book.png" alt="Card image cap" width="20" height="20">
+                        <h5 class="card-title mt-4 news-title">BM&DC MBBS Curriculum Update- 2012</h5>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <div><a href="#"><i class="fas fa-download"></i></a> </div>
+                        <div><a href="#">Read More</a></div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-3">
+                <div class="card  box-shadow border border bg-light" >
+                    <div class="card-body">
+                        <img class="card-img-top" src="../img/book.png" alt="Card image cap" width="20" height="20">
+                        <h5 class="card-title mt-4 news-title">BM&DC MBBS Curriculum Update- 2012</h5>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <div><a href="#"><i class="fas fa-download"></i></a> </div>
+                        <div><a href="#">Read More</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
 
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{asset('css')}}/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css')}}/all.min.css" rel="stylesheet">{{-- font awesome icon--}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="{{asset('css')}}/style.css" rel="stylesheet">
 
